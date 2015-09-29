42e99af74729efcc10edd8e53a7b0056958e80ca
Peter Wilson <peter.wilson@exari.com>
2015-09-29 10:49:57 +1000

    Incorporate @azaozz suggestions

--- js/srcset-feature-check.js
+++ js/srcset-feature-check.js
@@ -1,26 +1,13 @@
-(function( window ){
-	var document = window.document;
-	var config = window._wpResponsiveImagesSettings;
-	var image  = new window.Image();
-
-	function browserSupportsResponsiveImages() {
-		// Adapted from picturefill 2.3.1
-		if ( 'sizes' in image && 'currentSrc' in image && 'srcset' in image ) {
-			return true;
-		}
-
-		return false;
+(function( document, config, image, script, firstScript ){
+	// Adapted from picturefill 2.3.1
+	if ( 'sizes' in image && 'currentSrc' in image && 'srcset' in image ) {
+		return;
 	}
 
-	function enqueueScript( src ) {
-		var script = document.createElement( 'script' );
-		var firstScript = document.getElementsByTagName( 'script' )[0];
+	script = document.createElement( 'script' );
+	firstScript = document.getElementsByTagName( 'script' )[0];
 
-		script.async = script.src = src;
-		firstScript.parentNode.insertBefore( script, firstScript );
-	}
+	script.async = script.src = config.picturefillSource;
+	firstScript.parentNode.insertBefore( script, firstScript );
 
-	if ( false === browserSupportsResponsiveImages() ) {
-		enqueueScript( config.picturefillSource );
-	}
-})( window );
+})( document, _wpResponsiveImagesSettings, new Image() );
